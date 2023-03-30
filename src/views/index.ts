57b713f6a898239899f3019e6ff253de82ca9cd7
sd <leowest1921@gmail.com>
2023-03-30 15:06:37 +0800

    add

--- src/views/index.ts
+++ src/views/index.ts
@@ -1,10 +1,10 @@
 import type { RouteComponent } from "vue-router";
-//import "./_builtin/403/index.vue";
+
 export const views: Record<
 	PageRoute.LastDegreeRouteKey,
 	RouteComponent | (() => Promise<{ default: RouteComponent }>)
 > = {
-	403: () =>  import("./_builtin/403/index.vue") ,//import("./_builtin/403/index.vue"),
+	403: () => import("./_builtin/403/index.vue"),
 	404: () => import("./_builtin/404/index.vue"),
 	500: () => import("./_builtin/500/index.vue"),
 	"constant-page": () => import("./_builtin/constant-page/index.vue"),
