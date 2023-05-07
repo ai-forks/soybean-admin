c61d0d8750e8e137638bd947a5040c937003a395
sd <leowest1921@gmail.com>
2023-05-07 22:18:28 +0800

    add

---
+++ src/views/admin/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>{{ data.title }}</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+const data = ref({ title: "demo" });
+</script>
+
+<style scoped lang="scss"></style>
