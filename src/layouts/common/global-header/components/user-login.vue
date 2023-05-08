a603753d424ba834b4906a1790e7a147a44507a1
sd <leowest1921@gmail.com>
2023-05-08 08:15:55 +0800

    add

---
+++ src/layouts/common/global-header/components/user-login.vue
@@ -0,0 +1,18 @@
+<template>
+  <hover-container class="px-12px" :inverted="theme.header.inverted">
+    <router-link to="/login">
+      <icon-mdi-login-variant class="text-32px" />
+    </router-link>
+  </hover-container>
+</template>
+
+<script lang="ts" setup>
+import { useThemeStore } from "@/store";
+
+defineOptions({ name: "UserLogin" });
+
+const theme = useThemeStore();
+
+</script>
+
+<style scoped></style>
